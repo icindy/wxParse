170409897a356ae343684caacc8a106b6657b6b2
ismoker <uestczhangdi@163.com>
2016-11-10 11:55:18 +0800

    1.增加&middot;支持

--- wxParse/wxDiscode.js
+++ wxParse/wxDiscode.js
@@ -95,7 +95,7 @@ function strGreeceDiscode(str){
     str = str.replace(/&thetasym;/g, 'ϑ');
     str = str.replace(/&upsih;/g, 'ϒ');
     str = str.replace(/&piv;/g, 'ϖ');
-
+    str = str.replace(/&middot;/g, '·');
     return str;
 }
 
