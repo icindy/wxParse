6ce7645fbbfe5fe604806cd4a4d7f092fd5b2fbb
ismoker <uestczhangdi@163.com>
2016-11-14 16:25:28 +0800

    多数据解析

--- pages/index/index.js
+++ pages/index/index.js
@@ -30,6 +30,17 @@ Page({
 
     WxParse.wxParse('html',html,that)
 
+    var text = '# wxParse-微信小程序富文本解析自定义组件，支持HTML及markdown解析 \n'
+    +' > 微信小程序富文本解析自定义组件，支持HTML及markdown解析 \n\n' 
+    +'## 功能列表 \n * 动态加载代码  \n * html转json \n * markdown转html转json \n * 富文本markdown解析 \n * 自定义层级解析 \n * 自定义样式表 \n'
+    +'## 文件作用 \n'
+    +' ``` // wxParse.wxml //用于解析使用的模版 ``` \n'
+    +'## 开发信息 \n '
+    +' [微信小程序开发论坛](http://weappdev.com) \n'
+    +' ![微信小程序logo](http://weappdev.com/uploads/default/original/1X/9156b32bd04323f35d0957796f126b8a54595c97.png)';
+    WxParse.wxMoreParse('testData','md',text,that)
+    
+
   },
   wxParseImageLoad: function(e){
     
--- wxParse/wxParse.js
+++ wxParse/wxParse.js
@@ -75,7 +75,50 @@ function wxParse(type,data,target){
 
 // 多html或md处理入口函数
 
+function wxMoreParse(bindData,type,data,target){
+  wx.showToast({
+    title: '加载中....',
+    icon: 'loading',
+    duration: 10000
+  })
+  var that = target;
+  var wxParseData = [];
+  if(type == 'json'){
+    var json = HtmlToJson.html2json(html);
+	  console.log(JSON.stringify(json, ' ', ' '));
+    wxParseData = json.child;
+  }else if(type == 'html'){
+    data = wxDiscode.strDiscode(data);
+    var json = HtmlToJson.html2json(data);
+	  console.log(JSON.stringify(json, ' ', ' '));
+    wxParseData = json.child;
+  }else if(type == 'md' || type == 'markdown'){
+    var converter = new showdown.Converter();
+    var html      = converter.makeHtml(data);
+    html = wxDiscode.strDiscode(html);
+    var json = HtmlToJson.html2json(html);
+	  console.log(JSON.stringify(json, ' ', ' '));
+    wxParseData = json.child;
+  }
+  var wxParseImageObjArray = HtmlToJson.wxImageArray();
+  imgUrlArr = [];
 
+  for(var i=0;i<wxParseImageObjArray.length;i++){
+    var node = wxParseImageObjArray[i];
+    var imgUrl = node.attr.src;
+    imgUrl = wxDiscode.urlToHttpUrl(imgUrl,__placeImgeUrlHttps);
+    imgUrlArr.push(imgUrl);
+  }
+  var moreData = {};
+  moreData[bindData] = {
+    tagArray:wxParseData,
+    wxParseImgArray:imgUrlArr,
+    wxParseImageObjArray:wxParseImageObjArray 
+  }
+  // //更新数据
+  that.setData(moreData)
+  calImageInfo(0,that,bindData);
+}
 
 /**
  * 图片视觉宽高计算函数区 
@@ -146,6 +189,77 @@ function wxAutoImageCal(originalWidth,originalHeight){
 
   }
 
+
+/**
+ * 图片视觉宽高计算函数区 
+ **/
+
+// 假循环获取计算图片视觉最佳宽高
+function calMoreImageInfo(idx,that,bindData){
+  if(that.data[bindData].wxParseImageObjArray.length == 0){
+      return;
+  }
+  var temWxParseImageObjArray = that.data[bindData].wxParseImageObjArray;
+  var imgUrl = temWxParseImageObjArray[idx].attr.src;
+  wx.getImageInfo({
+      src: imgUrl,
+      success: function (res) {
+        // console.log(i+"width:"+res.width);
+        // console.log("height:"+res.height);
+        var recal = wxAutoImageCal(res.width,res.height);
+        temWxParseImageObjArray[idx].width = recal.imageWidth;
+        temWxParseImageObjArray[idx].height = recal.imageheight; 
+        that.setData({
+          wxParseImageObjArray:temWxParseImageObjArray 
+        })
+
+        if(idx == temWxParseImageObjArray.length-1){
+          temimgInfoArray = temWxParseImageObjArray;
+          
+          //更新数据
+          that.setData({
+            wxParseData:clone(that.data.wxParseData)
+          })
+          wx.hideToast()
+        }else{
+          calImageInfo(idx+1,that);
+        }
+      }
+    })
+}
+
+// 计算视觉优先的图片宽高
+function wxMoreAutoImageCal(originalWidth,originalHeight){
+    //获取图片的原始长宽
+    var windowWidth = 0,windowHeight = 0;
+    var autoWidth = 0,autoHeight = 0;
+    var results= {};
+    wx.getSystemInfo({
+      success: function(res) {
+        console.dir(res);
+        windowWidth = res.windowWidth;
+        windowHeight = res.windowHeight;
+        //判断按照那种方式进行缩放
+        console.log("windowWidth"+windowWidth);
+        if(originalWidth > windowWidth){//在图片width大于手机屏幕width时候
+          autoWidth = windowWidth;
+          console.log("autoWidth"+autoWidth);
+          autoHeight = (autoWidth*originalHeight)/originalWidth;
+          console.log("autoHeight"+autoHeight);
+          results.imageWidth = autoWidth;
+          results.imageheight = autoHeight;
+        }else{//否则展示原来的数据
+          results.imageWidth = originalWidth;
+          results.imageheight = originalHeight;
+        }
+      }
+    })
+
+    return results;
+
+  }
+
+
 // 用于深度更换图片的宽高
 function clone(obj) {
     var o;
@@ -186,6 +300,7 @@ function wxParseImgTap(e,that){
 
 module.exports = {
   wxParse:wxParse,
+  wxMoreParse:wxMoreParse,
   wxParseImgTap:wxParseImgTap
 }
 
