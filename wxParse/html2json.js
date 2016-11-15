def918d156cae0dff872517e4b679782bdfc9b6a
ismoker <uestczhangdi@163.com>
2016-11-15 16:09:12 +0800

    调整因/n br引起的问题

--- wxParse/html2json.js
+++ wxParse/html2json.js
@@ -16,10 +16,10 @@
   // Empty Elements - HTML 5
 	var empty = makeMap("area,base,basefont,br,col,frame,hr,img,input,link,meta,param,embed,command,keygen,source,track,wbr");
 	// Block Elements - HTML 5
-	var block = makeMap("a,code,address,article,applet,aside,audio,blockquote,button,canvas,center,dd,del,dir,div,dl,dt,fieldset,figcaption,figure,footer,form,frameset,h1,h2,h3,h4,h5,h6,header,hgroup,hr,iframe,ins,isindex,li,map,menu,noframes,noscript,object,ol,output,p,pre,section,script,table,tbody,td,tfoot,th,thead,tr,ul,video");
+	var block = makeMap("br,a,code,address,article,applet,aside,audio,blockquote,button,canvas,center,dd,del,dir,div,dl,dt,fieldset,figcaption,figure,footer,form,frameset,h1,h2,h3,h4,h5,h6,header,hgroup,hr,iframe,ins,isindex,li,map,menu,noframes,noscript,object,ol,output,p,pre,section,script,table,tbody,td,tfoot,th,thead,tr,ul,video");
 
 	// Inline Elements - HTML 5
-	var inline = makeMap("abbr,acronym,applet,b,basefont,bdo,big,br,button,cite,del,dfn,em,font,i,iframe,img,input,ins,kbd,label,map,object,q,s,samp,script,select,small,span,strike,strong,sub,sup,textarea,tt,u,var");
+	var inline = makeMap("abbr,acronym,applet,b,basefont,bdo,big,button,cite,del,dfn,em,font,i,iframe,img,input,ins,kbd,label,map,object,q,s,samp,script,select,small,span,strike,strong,sub,sup,textarea,tt,u,var");
 
 	// Elements that you can, intentionally, leave open
 	// (and which close themselves)
--- wxParse/wxDiscode.js
+++ wxParse/wxDiscode.js
@@ -173,7 +173,8 @@ function strOtherDiscode(str){
 
 function strMoreDiscode(str){
     str = str.replace(/\r\n/g,"");  
-    str = str.replace(/\n\n/g,"\n");
+    str = str.replace(/\n/g,"");
+
     str = str.replace(/code/g,"wxxxcode-style");
     return str;
 }
